1709605ae2ad68b2f7bc0aa2975f5b3f5de1d96c
MajorDown <romain.fouillaron@gmx.fr>
2023-11-15 21:16:50 +0100

    mise en place de l'environnement de développement

---
+++ buzr/App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+import { useFonts } from "expo-font";
+import styles from "./styles/styles";
+
+export default function App() {
+
+  const [loadedFont, FontError] = useFonts({
+    "Orbitron-Bold": require("./assets/fonts/Orbitron-Bold.ttf"),
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+  });
+  if (FontError) return <Text>loading...</Text>;
+  if (loadedFont)
+    return (
+      <View style={styles.app}>
+        <StatusBar style="light" />
+      </View>
+    );
+}
